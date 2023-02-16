68314b11c03f70731bfb5f9a8d8ee457b721d9ce
Parker Barthlome <pbb@amazon.com>
2023-02-16 11:18:44 -0700

    random url

--- data.tf
+++ data.tf
@@ -10,8 +10,9 @@ data "aws_eks_cluster" "cluster" {
 data "http" "eks_cluster_readiness" {
   count = var.create_eks ? 1 : 0
 
-  url            = join("/", [data.aws_eks_cluster.cluster[0].endpoint, "healthz"])
-  ca_certificate = base64decode(data.aws_eks_cluster.cluster[0].certificate_authority[0].data)
+  #url            = join("/", [data.aws_eks_cluster.cluster[0].endpoint, "healthz"])
+  #ca_certificate = base64decode(data.aws_eks_cluster.cluster[0].certificate_authority[0].data)
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
   timeout        = var.eks_readiness_timeout
 }
 
